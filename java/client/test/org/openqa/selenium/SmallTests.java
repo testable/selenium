92eb92928f08f6d2fba265dafba1345b2d16933f
Simon Stewart <simon.m.stewart@gmail.com>
2017-01-11 16:21:35 +0300

    Flip the Actions class to use the W3C interactions if present

--- java/client/test/org/openqa/selenium/SmallTests.java
+++ java/client/test/org/openqa/selenium/SmallTests.java
@@ -16,7 +16,6 @@
 // under the License.
 package org.openqa.selenium;
 
-import org.openqa.selenium.interactions.ActionsTest;
 import org.openqa.selenium.interactions.CompositeActionTest;
 import org.openqa.selenium.interactions.IndividualKeyboardActionsTest;
 import org.openqa.selenium.interactions.IndividualMouseActionsTest;
@@ -36,7 +35,6 @@ import org.openqa.selenium.testing.drivers.IgnoreComparitorUnitTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    ActionsTest.class,
     ByTest.class,
     CommandLineTest.class,
     CookieTest.class,
